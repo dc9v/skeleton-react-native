5fd274afbee9084b489b56f233efa2144a84b273
Taegyun, Ko <dev@balsa.to>
2022-12-16 15:13:01 +0900

    updated /index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import { AppRegistry, LogBox } from 'react-native';
-import "react-native-gesture-handler";
-import "reflect-metadata";
-import { name as appName } from './app.json';
+import { name as AppName } from './app.json';
 import App from "/src/main/index";
 
 LogBox.ignoreAllLogs();
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(AppName, () => App);

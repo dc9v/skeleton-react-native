2c5d65002caa42f0fb1eaa0ae56a1d8e6da5d88e
Taegyun, Ko <dev@balsa.to>
2022-12-16 15:11:55 +0900

    updated statusbar style

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,11 +1,33 @@
-import React, { useMemo } from "react";
-import { View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import MainScreen from "/src/main/screens/MainScreen";
+import * as React from 'react';
+import { v4 as uuid4 } from 'uuid';
+import ElementUtils from '../utils/ElementUtils';
+import SecondayScreen from './screens/SecondayScreen';
+import { StatusBar } from 'react-native';
 
-interface ProfileScreenProps { }
-
-const ProfileScreen: React.FC<any> = () =>
+const App = () =>
 {
-  return (<View> </View>);
+  const Stack = createNativeStackNavigator();
+  const options: NativeStackNavigationOptions = { headerShown: false, statusBarStyle: "dark", statusBarColor: "transparent", statusBarAnimation: "slide", statusBarTranslucent: true };
+
+  /* const [ text, setText ] = React.useState("");
+  const [ rootState, setRootState ] = React.useState(""); */
+
+  React.useEffect(() =>
+  {
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen key={ ElementUtils.keygen() } name={ "MainScreen" } component={ MainScreen } options={ { ...options, animation: 'fade' } } />
+        <Stack.Screen key={ ElementUtils.keygen() } name={ "SecondayScreen" } component={ SecondayScreen } options={ { ...options, animation: 'fade' } } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-export default ProfileScreen;
+export { App as default };
+
